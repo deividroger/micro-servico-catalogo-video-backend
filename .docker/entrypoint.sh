087784adf07cca13b170422f50a46a3f85387c7d
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2020-11-20 21:43:12 -0300

    ajustando projeto para executar via CI

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+
 chown -R www-data:www-data .
+
+cp .env.example .env
+cp .env.testing.example .env.testing
 composer install
 php artisan key:generate
 php artisan migrate
