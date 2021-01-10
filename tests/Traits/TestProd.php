a45fdb74aed28294306a4809edfc307235efec6f
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-01-10 16:12:33 -0300

    testando com google cloud store

---
+++ tests/Traits/TestProd.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Traits;
+
+trait TestProd {
+ protected function skipTestIfNotProd($message ='')   {
+    if(!$this->isTestingProd()){
+        $this->markTestSkipped($message);
+    }
+ }
+
+ protected function isTestingProd(){
+     return env('TESTING_PROD') !== false;
+ }
+
+}
