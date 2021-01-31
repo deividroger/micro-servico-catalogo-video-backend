2082abc82a9936bc3eceec892be36ef0a154f6c5
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-01-31 14:34:00 -0300

    componente de breadcrumb

--- frontend/src/components/Breadcrumbs.tsx
+++ frontend/src/components/Breadcrumbs.tsx
@@ -1,60 +1,31 @@
 /* eslint-disable no-nested-ternary */
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
 import Link, { LinkProps } from '@material-ui/core/Link';
-import ListItem from '@material-ui/core/ListItem';
-import Collapse from '@material-ui/core/Collapse';
-import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
 import MuiBreadcrumbs from '@material-ui/core/Breadcrumbs';
-import { Route, MemoryRouter } from 'react-router';
+import { Route } from 'react-router';
 import { Link as RouterLink } from 'react-router-dom';
-import { Omit } from '@material-ui/types';
+import { Location } from 'history'
+import routes from '../routes';
+import RouteParser from 'route-parser';
+import { Container } from '@material-ui/core';
 
-interface ListItemLinkProps extends LinkProps {
-  to: string;
-  open?: boolean;
-}
-
-const breadcrumbNameMap: { [key: string]: string } = {
-  '/inbox': 'Inbox',
-  '/inbox/important': 'Important',
-  '/trash': 'Trash',
-  '/spam': 'Spam',
-  '/drafts': 'Drafts',
-};
-
-function ListItemLink(props: Omit<ListItemLinkProps, 'ref'>) {
-  const { to, open, ...other } = props;
-  const primary = breadcrumbNameMap[to];
-
-  return (
-    <li>
-      <ListItem button component={RouterLink} to={to} {...other}>
-        <ListItemText primary={primary} />
-        {open != null ? open ? <ExpandLess /> : <ExpandMore /> : null}
-      </ListItem>
-    </li>
-  );
-}
+const breadcrumbNameMap: { [key: string]: string } = {}
+routes.forEach(route => breadcrumbNameMap[route.path as string] = route.label);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      width: 360,
-    },
-    lists: {
-      backgroundColor: theme.palette.background.paper,
-      marginTop: theme.spacing(1),
-    },
-    nested: {
-      paddingLeft: theme.spacing(4),
-    },
+    
+    linkRouter: {
+      color: '#4db5ab', 
+      "&:focus, &:active":{
+        color: '#4db5ab', 
+      },
+      "&:hover": {
+        color: "#055a52"
+      }
+    }
   }),
 );
 
@@ -65,49 +36,51 @@ interface LinkRouterProps extends LinkProps {
 
 const LinkRouter = (props: LinkRouterProps) => <Link {...props} component={RouterLink as any} />;
 
-export default function  Breadcrumbs() {
-  
-  const classes = useStyles();
-  const [open, setOpen] = useState(true);
+export default function Breadcrumbs() {
 
-  const handleClick = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
+  const classes = useStyles();
+
+  function makeBreakcrumb(location: Location) {
+
+    const pathnames = location.pathname.split('/').filter((x) => x);
+
+    pathnames.unshift('/');
+
+    return (
+      <MuiBreadcrumbs aria-label="breadcrumb">
+
+        {pathnames.map((value, index) => {
+          const last = index === pathnames.length - 1;
+
+          const to = `${pathnames.slice(0, index + 1).join('/').replace('//', '/')}`;
+
+          const route = Object.keys(breadcrumbNameMap).find(path => new RouteParser(path).match(to));
+
+          if (route === undefined) {
+            return false;
+          }
+          return last ? (
+            <Typography color="textPrimary" key={to}>
+              {breadcrumbNameMap[route]}
+            </Typography>
+          ) : (
+              <LinkRouter color="inherit" to={to} key={to} className={classes.linkRouter}>
+                {breadcrumbNameMap[route]}
+              </LinkRouter>
+            );
+        })}
+      </MuiBreadcrumbs>
+    );
+  }
 
   return (
-    <MemoryRouter initialEntries={['/inbox']} initialIndex={0}>
-      <div className={classes.root}>
-        <Route>
-          {({ location }) => {
-            const pathnames = location.pathname.split('/').filter((x) => x);
+    <Container>
+      <Route>
+        {
+          ({ location }: { location: Location }) => makeBreakcrumb(location)
+        }
+      </Route>
+    </Container>
 
-            return (
-              <MuiBreadcrumbs aria-label="breadcrumb">
-                <LinkRouter color="inherit" to="/">
-                  Home
-                </LinkRouter>
-                {pathnames.map((value, index) => {
-                  const last = index === pathnames.length - 1;
-                  const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-
-                  return last ? (
-                    <Typography color="textPrimary" key={to}>
-                      {breadcrumbNameMap[to]}
-                    </Typography>
-                  ) : (
-                    <LinkRouter color="inherit" to={to} key={to}>
-                      {breadcrumbNameMap[to]}
-                    </LinkRouter>
-                  );
-                })}
-              </MuiBreadcrumbs>
-            );
-          }}
-        </Route>
-        
-      </div>
-    </MemoryRouter>
   );
-
-  
 }
