d4926fa32e21053a3d5062dcdf7ad7689b85f39b
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-07-09 20:19:18 -0300

    componente de rating

--- frontend/src/pages/video/PageForm.tsx
+++ frontend/src/pages/video/PageForm.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import {Form} from "./Form";
+
 import {Page} from "../../components/Page";
 import {useParams} from 'react-router';
+import { Form } from './Form/Index';
 
 const PageForm = () => {
     
